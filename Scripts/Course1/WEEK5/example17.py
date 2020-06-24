da91e204d7e90d26d6442564ac0bb052625ea4c6
Aleksey Starostin <a-starostin@mail.ru>
2020-06-24 08:31:31 +0300

    продолжил изучение acyncio

--- Scripts/Course1/WEEK5/example17.py
+++ Scripts/Course1/WEEK5/example17.py
@@ -4,15 +4,16 @@ import asyncio
 
 
 async def handle_echo(reader, writer):
-    data=await reader.read(1024)
-    message=data.decode()
-    addr=writer.get_extra_info("peername")
-    print("received %r from %r" % (message,addr))
+    data = await reader.read(1024)
+    message = data.decode()
+    addr = writer.get_extra_info("peername")
+    print("received %r from %r" % (message, addr))
     writer.close()
 
-loop=asyncio.get_event_loop()
-coro=asyncio.start_server(handle_echo, "127.0.0.1", 10001, loop=loop)
-server=loop.run_until_complete(coro)
+
+loop = asyncio.get_event_loop()
+coro = asyncio.start_server(handle_echo, "127.0.0.1", 10001, loop=loop)
+server = loop.run_until_complete(coro)
 try:
     loop.run_forever()
 except KeyboardInterrupt:
